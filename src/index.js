6f27ff5c5fd96391887035285c48335fb7452402
John Vall <jvall@westmonroepartners.com>
2017-05-18 13:58:16 -0500

    lolz

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 
-import { Observable } from "rxjs/Observable";
 import { Provider } from "react-redux";
 
 import configureStore from "./store/configureStore";
