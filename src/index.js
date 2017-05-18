915031d3e2cf88ec581d87ddc65f6739e883dbed
John Vall <jvall@westmonroepartners.com>
2017-05-18 13:48:13 -0500

    another test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import "./index.css";
 
 import { Observable } from "rxjs/Observable";
-
 import { Provider } from "react-redux";
 
 import configureStore from "./store/configureStore";
