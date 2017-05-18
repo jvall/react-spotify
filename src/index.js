fc6c59397a30268f1b541b2955697a9bd7a73ff8
John Vall <jvall@westmonroepartners.com>
2017-05-18 12:47:34 -0500

    linting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
 
-import {Provider} from 'react-redux'
+import { Provider } from "react-redux";
 
-import configureStore from './store/configureStore';
-import App from './components/App';
-import SearchArtists from './containers/SearchArtists/SearchArtists.jsx';
-import LikedArtists from './containers/LikedArtists/LikedArtists.jsx';
+import configureStore from "./store/configureStore";
+import App from "./components/App";
+import SearchArtists from "./containers/SearchArtists/SearchArtists.jsx";
+import LikedArtists from "./containers/LikedArtists/LikedArtists.jsx";
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-  <App/>
-</Provider>, document.getElementById('root'));
+  <Provider store={store}> <App /> </Provider>,
+  document.getElementById("root")
+);
