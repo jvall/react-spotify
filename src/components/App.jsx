e8f200992ffdf40c5c16a24f3fa929951cd7fa6e
John Vall <jvall@westmonroepartners.com>
2017-05-18 12:45:16 -0500

    test with linting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,28 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navigation from './Navigation/Navigation.jsx';
-import SearchArtists from '../containers/SearchArtists/SearchArtists.jsx';
-import LikedArtists from '../containers/LikedArtists/LikedArtists.jsx';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./Navigation/Navigation.jsx";
+import SearchArtists from "../containers/SearchArtists/SearchArtists.jsx";
+import LikedArtists from "../containers/LikedArtists/LikedArtists.jsx";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
   render() {
     return (
       <Router>
         <div className="App">
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
+            <img src={logo} className="App-logo" alt="logo" />
             <h2>Welcome to React Spotify App</h2>
             <Navigation />
           </div>
           <Switch>
             <Route exact name="index" path="/" component={SearchArtists} />
-            <Route name="liked-artists" path="/liked-artists" component={LikedArtists} /> 
+            <Route
+              name="liked-artists"
+              path="/liked-artists"
+              component={LikedArtists}
+            />
           </Switch>
         </div>
       </Router>
